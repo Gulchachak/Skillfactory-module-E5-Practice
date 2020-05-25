bd946ddb0d31a3ad4c5603f5d34d860e2e8d4168
Gulchachak <ilkaeva85@rambler.ru>
2020-05-25 03:32:20 +0000

    update settings

--- autocatalog/settings.py
+++ autocatalog/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'autocatalog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DB_NAME"),
+	'USER': os.getenv("DB_USER"),
+	'PASSWORD': os.getenv("DB_PASSWORD"),
+	'HOST': os.getenv("DB_HOST", "127.0.0.1"),
+	'PORT': os.getenv("DB_PORT", 5432),
     }
 }
 
@@ -122,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT= os.path.join(BASE_DIR, 'static/')
