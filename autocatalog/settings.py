273e5a0120c4e5cd226e89b9a790602a3a10206d
Gulchachak <ilkaeva85@rambler.ru>
2020-05-25 19:12:26 +0000

    update settings

--- autocatalog/settings.py
+++ autocatalog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h5b_@vek6@0tjlfj+#n2yxwqif)gi96018y6l&zsvw)jt$mbj='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
